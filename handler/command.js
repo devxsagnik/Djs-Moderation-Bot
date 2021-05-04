1d0773123b13d326821c99565245bc1391761786
gtagamermods <73096657+gtagamermods@users.noreply.github.com>
2021-05-04 13:39:18 +0530

    New Update

--- handler/command.js
+++ handler/command.js
@@ -9,5 +9,5 @@ module.exports = (bot) => {
             if (pull.config.aliases) pull.config.aliases.forEach(a => bot.aliases.set(a, pull.config.name));
           };
         };
-        ["admin", "info", "mod", "utility"].forEach(x => load(x));
+        ["admin", "fun", "images", "info", "mod", "utility"].forEach(x => load(x));
 };
