d6793f3786b88426da9074bf95498b5c9d211dd6
Felix_PlaYz <felix.playz1407@gmail.com>
2021-11-05 13:08:47 +0530

    Update wanted.js

--- commands/Images/wanted.js
+++ commands/Images/wanted.js
@@ -1,37 +1,37 @@
-const db = require("old-wio.db");
+const Discord = require("discord.js");
 const {
-  Client,
-  Message,
-  MessageAttachment,
-  MessageEmbed,
+    Client,
+    Message,
+    MessageAttachment
 } = require("discord.js");
-const { Canvas } = require("canvacord");
+const {
+    Canvas
+} = require("canvacord");
 
 module.exports = {
-  name: "wasted",
-  category: "Images",
-  aliases: [" "],
-  description: "Get wasted",
-  usage: "[command]",
-  cooldown: 1, //the command cooldown for execution & for helpcmd [OPTIONAL]
-  memberpermissions: [], //Only allow members with specific Permissions to execute a Commmand [OPTIONAL]
-  requiredroles: [], //Only allow specific Users with a Role to execute a Command [OPTIONAL]
-  alloweduserids: [], //Only allow specific Users to execute a Command [OPTIONAL]
+    name: "wanted",
+    category: "Images",
+    description: "Make yourself wanted or anyone",
+    aliases: [""],
+    usage: "wanted",
+    cooldown: 1, //the command cooldown for execution & for helpcmd [OPTIONAL]
+    memberpermissions: [], //Only allow members with specific Permissions to execute a Commmand [OPTIONAL]
+    requiredroles: [], //Only allow specific Users with a Role to execute a Command [OPTIONAL]
+    alloweduserids: [], //Only allow specific Users to execute a Command [OPTIONAL]
 
-  run: async (client, message, args) => {
-    const user = message.mentions.users.first() || message.author;
+    run: async (client, message, args) => {
+        const user = message.mentions.users.first() || message.author;
 
-    const avatar = user.displayAvatarURL({
-      format: "png"
-    });
+        const avatar = user.displayAvatarURL({
+            format: "png"
+        });
 
-    const image = await Canvas.wasted(avatar);
+        const image = await Canvas.wanted(avatar);
 
-    let attach = new MessageAttachment(image, "wasted.png");
+        let acct = new MessageAttachment(image, "wanted.png");
 
-    message.channel.send({
-      files: [attach]
-    });
-
-  }
+        message.channel.send({
+            files: [acct]
+        });
+    }
 }
